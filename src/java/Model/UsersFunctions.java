01b9ff7bcf7d2c5a2231a3ab6ab77fd384577366
KGRS <KGRS@192.168.1.2>
2017-04-29 18:23:23 +0530

    By Ravindu 2017-04-29

---
+++ src/java/Model/UsersFunctions.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import db.Users;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author KGRS
+ */
+public class UsersFunctions {
+    
+    private final SessionFactory sf;
+    
+    public UsersFunctions(){
+        sf = conn.HibernateUtil.getSessionFactory();
+    }
+    
+    public static void main(String[] args) {
+        new UsersFunctions().selectUsers();
+    }
+    
+    private List selectUsers(){
+        Session ses = sf.openSession();
+        Criteria cr = ses.createCriteria(UsersFunctions.class);
+        List<Users> u = cr.list();
+//        for (Users u1 : u) {
+//            System.out.println(u1.getUsername());
+//        }        
+        return u;
+    }
+    
+    private Users selectUser(int userid) throws Exception{
+        Session ses = sf.openSession();
+        Criteria cr = ses.createCriteria(UsersFunctions.class);
+        cr.add(Restrictions.eq("userd", userid));
+        return (Users) cr.uniqueResult();        
+    }
+    
+    private void insertUser(){
+        
+    }
+}
